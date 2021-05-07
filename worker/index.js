5d0363b546b7f3289b0029f52a089f8c6cb363c0
Wolf <zevrov.719@gmail.com>
2021-05-07 12:33:21 -0400

    setup github fetching

--- worker/index.js
+++ worker/index.js
@@ -3,6 +3,4 @@ var CronJob = require('cron').CronJob;
 const fetchGithub = require('./tasks/fetch-github');
 
 // fetch Github jobs
-new CronJob('*/1 * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+new CronJob('*/1 * * * *', fetchGithub, null, true, 'America/Los_Angeles');
